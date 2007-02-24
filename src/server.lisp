03ad32b7289b3dc2f682e66a110515eb416db42e
coffeemug <coffeemug@gmail.com>
2007-02-24 16:44:38 -0500

    A sample page

---
+++ src/server.lisp
@@ -0,0 +1,18 @@
+
+(in-package :weblocks)
+
+(defvar *weblocks-server* nil)
+
+(defun start-weblocks ()
+  (if (null *weblocks-server*)
+      (setf *weblocks-server* (start-server :port 8080))))
+
+(defun stop-weblocks ()
+  (if (not (null *weblocks-server*))
+      (stop-server *weblocks-server*)))
+
+(defun hala ()
+  (get-output-stream-string (render-data *joe-employee*)))
+
+(setf *dispatch-table*
+      (cons (create-prefix-dispatcher "/test" 'hala) *dispatch-table*))
