5721c1f817f9975b45471bf19a132a359b804f71
coffeemug <coffeemug@gmail.com>
2007-07-01 22:21:44 -0400

    Finalizing port to IE (layout and other minor issues)

--- src/page-template.lisp
+++ src/page-template.lisp
@@ -11,6 +11,7 @@
     (:html :xmlns "http://www.w3.org/1999/xhtml"
      (:head
       (:title "Hello!")
+      (:link :rel "stylesheet" :type "text/css" :href "/pub/layout.css")
       (:link :rel "stylesheet" :type "text/css" :href "/pub/main.css")
       (:link :rel "stylesheet" :type "text/css" :href "/pub/flash.css")
       (:link :rel "stylesheet" :type "text/css" :href "/pub/navigation.css")
@@ -21,7 +22,6 @@
       (:link :rel "stylesheet" :type "text/css" :href "/pub/table.css")
       (:link :rel "stylesheet" :type "text/css" :href "/pub/datagrid.css")
       (:link :rel "stylesheet" :type "text/css" :href "/pub/gridedit.css")
-      (:link :rel "stylesheet" :type "text/css" :href "/pub/layout.css")
       (when *render-debug-toolbar*
 	(htm (:link :rel "stylesheet" :type "text/css" :href "/pub/debug-mode.css")))
       ; empty quote in script tags are a fix for w3m
@@ -43,5 +43,6 @@ render extra html prior and post the page wrapper."))
 (defmethod render-page-body (body-fn)
   (with-html
     (:div :class "page-wrapper"
-	  (with-extra-tags
-	    (funcall body-fn)))))
+	  (render-extra-tags "page-extra-top-" 3)
+	  (funcall body-fn)
+	  (render-extra-tags "page-extra-bottom-" 3))))
