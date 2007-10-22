ae4eb24367f958d57c1d8d3f73da3b29d743d7cd
coffeemug <coffeemug@gmail.com>
2007-10-21 23:38:20 -0400

    Adding start/stop functions to generated apps

--- scripts/new-app-templates/{APPNAME}.lisp
+++ scripts/new-app-templates/{APPNAME}.lisp
@@ -6,3 +6,14 @@
 
 (in-package :{APPNAME})
 
+(export '(start-{APPNAME} stop-{APPNAME}))
+
+(defun start-{APPNAME} (&rest args)
+  "Starts the application by calling 'start-weblocks' with appropriate
+arguments."
+  (apply #'start-weblocks args))
+
+(defun stop-{APPNAME} ()
+  "Stops the application by calling 'stop-weblocks'."
+  (stop-weblocks))
+
