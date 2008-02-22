2b00a9e0cd20fbcf73f09898f1b85de7c1afe57f
coffeemug <coffeemug@gmail.com>
2008-02-22 00:49:04 -0500

    Fixing password presentation to work on null values

--- test/views/types/password.lisp
+++ test/views/types/password.lisp
@@ -10,6 +10,14 @@
 			      nil *joe*)
   (:input :type "password" :name "password" :maxlength "10" :class "password"))
 
+(deftest-html password-render-view-field-value-2
+    (render-view-field-value  nil (make-instance 'password-presentation)
+			      (make-instance 'form-view-field
+					     :slot-name 'password)
+			      (find-view '(form employee))
+			      nil *joe*)
+  (:input :type "password" :name "password" :maxlength "10" :class "password"))
+
 ;;; Test password print-view-field-value
 (deftest password-print-view-field-value-1
     (print-view-field-value  "foo" (make-instance 'password-presentation)
