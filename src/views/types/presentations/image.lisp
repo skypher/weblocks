48fa349da97a4d65a57d630403b0f14ec665eda0
coffeemug <coffeemug@gmail.com>
2008-04-30 17:24:15 -0400

    Adding support for width and height to image presentation

--- src/views/types/presentations/image.lisp
+++ src/views/types/presentations/image.lisp
@@ -2,7 +2,8 @@
 (in-package :weblocks)
 
 (export '(image image-presentation image-presentation-alt
-	  image-presentation-title))
+	  image-presentation-title image-presentation-width
+	  image-presentation-height))
 
 (defclass image-presentation (text-presentation)
   ((alt :initform nil
@@ -13,7 +14,15 @@
    (title :initform nil
 	  :initarg :title
 	  :accessor image-presentation-title
-	  :documentation "A title to be specified with the image."))
+	  :documentation "A title to be specified with the image.")
+   (width :initform nil
+	  :initarg :width
+	  :accessor image-presentation-width
+	  :documentation "A width attribute of the image.")
+   (height :initform nil
+	   :initarg :height
+	   :accessor image-presentation-height
+	   :documentation "A height attribute of the image."))
   (:documentation "Presents a url as an image."))
 
 (defmethod render-view-field-value (value (presentation image-presentation)
@@ -24,6 +33,8 @@
       (call-next-method)
       (with-html
 	(:img :src value
+	      :width (image-presentation-width presentation) 
+	      :height (image-presentation-height presentation) 
 	      :alt (image-presentation-alt presentation)
 	      :title (image-presentation-title presentation)))))
 
