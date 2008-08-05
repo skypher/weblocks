363da16fe12328c8dbae4c4f2a2b1425553007ae
Stephen Compall <scompall@nocandysw.com>
2008-08-05 01:11:15 -0500

    Partially fix with-page-1

--- test/page-template.lisp
+++ test/page-template.lisp
@@ -25,28 +25,30 @@
 	(page-title (weblocks::current-webapp))))
   "Webapp Name")
 
+(defwebapp some-name)
+
 ;;; test with-page
 (deftest-html with-page-1
-    (let ((weblocks::*render-debug-toolbar* nil)
-	  (weblocks::*page-dependencies* (mapcar (curry #'apply
-							(curry-after #'make-local-dependency :do-not-probe t))
-						 '((:stylesheet "foo")
-						   (:stylesheet "bar"))))
-	  (weblocks::*webapp-name* 'some-name)
-	  (*current-page-description* "Some Page"))
-      (declare (special weblocks::*page-dependencies*
-			*current-page-description* weblocks::*webapp-name*))
-      (with-html
-	(:div "test"))
-      (weblocks::render-page))
+    (with-webapp (:class-name 'some-name)
+      (let ((weblocks::*page-dependencies*
+	     (mapcar (curry #'apply
+			    (curry-after #'make-local-dependency :do-not-probe t))
+		     '((:stylesheet "foo")
+		       (:stylesheet "bar"))))
+	    (*current-page-description* "Some Page"))
+	(declare (special weblocks::*page-dependencies*
+			  *current-page-description*))
+	(with-html
+	  (:div "test"))
+	(weblocks::render-page (weblocks::current-webapp))))
   (htm
    (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" ")
    (str "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">")
    (fmt "~%")
    (:html :xmlns "http://www.w3.org/1999/xhtml"
     (:head
-     (:meta :http-equiv "Content-type" :content "text/html; charset=utf-8")
      (:title "Some Name - Some Page")
+     (:meta :http-equiv "Content-type" :content "text/html; charset=utf-8")
      (:link :rel "stylesheet" :type "text/css" :href "/pub/stylesheets/layout.css")
      (:link :rel "stylesheet" :type "text/css" :href "/pub/stylesheets/main.css")
      (:link :rel "stylesheet" :type "text/css" :href "/pub/stylesheets/dialog.css")
