d8a87c70c5d9db10b8829dd976bbf167013f3aa6
Stephen Compall <scompall@nocandysw.com>
2008-08-06 19:18:14 -0500

    Fix find-pane-{5,6}

--- test/widgets/navigation.lisp
+++ test/widgets/navigation.lisp
@@ -281,33 +281,33 @@
     (let ((nav (make-navigation "test navigation"
 				"test1" "w1"
 				"test2" "w2")))
-      (setf (navigation-on-find-pane nav)
+      (setf (dispatcher-on-dispatch nav)
 	    (let ((c 0))
 	      (lambda (nav name)
-		(when (and (equalp name "test3")
+		(when (and (equalp name '("test3"))
 			   (= c 0))
 		  (incf c)
-		  "w3"))))
-      (values (find-pane nav "test3")
-	      (find-pane nav "test3")
-	      (find-pane nav "test4")))
-  ("test3" . "w3") ("test3" . "w3") nil)
+		  (values "w3" name '())))))
+      (values-list
+       (mapcar (curry #'weblocks::dispatcher-get-widget nav)
+	       '(("test3") ("test3") ("test4")))))
+  "w3" "w3" nil)
 
 (deftest find-pane-6
     (let ((nav (make-navigation "test navigation"
 				"test1" "w1"
 				"test2" "w2")))
-      (setf (navigation-on-find-pane nav)
+      (setf (dispatcher-on-dispatch nav)
 	    (let ((c 0))
 	      (lambda (nav name)
-		(when (and (equalp name "test3")
+		(when (and (equalp name '("test3"))
 			   (= c 0))
 		  (incf c)
-		  (values "w3" :no-cache)))))
-      (values (find-pane nav "test3")
-	      (find-pane nav "test3")
-	      (find-pane nav "test4")))
-  ("test3" . "w3") nil nil)
+		  (values "w3" name '() :no-cache)))))
+      (values-list
+       (mapcar (curry #'weblocks::dispatcher-get-widget nav)
+	       '(("test3") ("test3") ("test4")))))
+  "w3" nil nil)
 
 ;;; test reset-current-pane
 (deftest reset-current-pane-1
