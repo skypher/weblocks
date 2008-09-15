53e2d580bd3477f201ca25b77b0580a7029dcc79
Stephen Compall <scompall@nocandysw.com>
2008-09-15 01:51:51 -0500

    adapt defwebapp-1

--- test/application.lisp
+++ test/application.lisp
@@ -5,19 +5,18 @@
   :description "foo bar")
 
 ;;; test defwebapp
-(deftest defwebapp-1
-    (with-webapp (:class-name 'hello-webapp)
-      (values (webapp-name)
-	      (mapcar (curry #'format nil "~A")
-		      (mapcar #'dependency-url (webapp-application-dependencies)))
-	      (webapp-description)))
-  "hello-webapp"
-  ("/pub/stylesheets/layout.css"
-   "/pub/stylesheets/main.css"
-   "/pub/stylesheets/dialog.css"
-   "/pub/scripts/prototype.js"
-   "/pub/scripts/scriptaculous.js"
-   "/pub/scripts/shortcut.js"
-   "/pub/scripts/weblocks.js"
-   "/pub/scripts/dialog.js")
-  "foo bar")
+(addtest defwebapp-1
+  (with-webapp (:class-name 'hello-webapp)
+    (ensure-same (webapp-name) "hello-webapp" :test string-equal)
+    (ensure-same (mapcar (curry #'format nil "~A")
+			 (mapcar #'dependency-url (webapp-application-dependencies)))
+		 '("/pub/stylesheets/layout.css"
+		   "/pub/stylesheets/main.css"
+		   "/pub/stylesheets/dialog.css"
+		   "/pub/scripts/prototype.js"
+		   "/pub/scripts/scriptaculous.js"
+		   "/pub/scripts/shortcut.js"
+		   "/pub/scripts/weblocks.js"
+		   "/pub/scripts/dialog.js")
+		 :test set-equal-equal)
+    (ensure-same (webapp-description) "foo bar")))
