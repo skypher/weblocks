7e71b122ee01909faad289c86bfd6ba751f1aa94
Stephen Compall <scompall@nocandysw.com>
2008-09-26 22:14:33 -0500

    CONTRIB: parameterize relative order of persistence in persist-children

--- contrib/s11001001/persist-children.lisp
+++ contrib/s11001001/persist-children.lisp
@@ -3,75 +3,81 @@
 (in-package #:weblocks-s11)
 
 (export '(persistent-children-mixin persistent-children add-child-to-persist
-	  persist-children persist-child))
+	  persist-children persist-child
+	  default-persist-child-order define-default-persist-child-order))
 
 (defgeneric persistent-children (self)
   (:documentation "Answer a list of objects to persist when persisting
-SELF, via `persist-child'.  It will be cleared after such persistence."))
-(defgeneric (setf persistent-children) (list self)
-  (:documentation "Alter the value of `persistent-children'.  You
-oughtn't use this.  Use `add-child-to-persist' instead."))
+SELF, via `persist-child'.  It will be cleared after such
+persistence."))
 
 (defclass persistent-children-mixin ()
-  ((children :type list :initform '() :accessor persistent-children)
-   (children-tail :type list :initform nil))
+  ((children :type hash-table :initform (make-hash-table :test 'eq)))
   (:documentation "Provide the `persistent-children' accessor and
 backing."))
 
-(defmethod (setf persistent-children) (list (self persistent-children-mixin))
-  "Obviously not too efficient, which is why you ought to take heed of
-the GF docstring."
-  (with-slots (children children-tail) self
-    (setf children list
-	  children-tail (last list))))
+(defmethod persistent-children ((self persistent-children-mixin))
+  (arnesi:hash-table-keys (slot-value self 'children)))
 
-(defgeneric add-child-to-persist (child self &optional position)
+(defgeneric add-child-to-persist (child self &optional order)
   (:documentation "Arrange for CHILD to be sent `persist-child' when
-persisting SELF.  POSITION may be of (member :before :after nil),
-determining where CHILD ought to be added in the list.  NIL means you
-don't care."))
+persisting SELF.  ORDER may be of (member :before :after t nil),
+determining when CHILD will be persisted relative to SELF.  NIL means
+use `default-persist-child-order'.  T means do it both times, on the
+off chance this means something."))
 
-(defmethod add-child-to-persist (child (self persistent-children-mixin)
-				 &optional position)
-  "Yes, we alter structure."
-  (with-slots (children children-tail) self
-    (ecase position
-      ((:before nil)
-	 (push child children)
-	 (unless children-tail
-	   (setf children-tail children)))
-      ((:after)
-	 (if children-tail
-	     (setf children-tail
-		   (setf (cdr children-tail) (list child)))
-	     (setf children (setf children-tail (list child))))))))
+(defmethod add-child-to-persist
+    (child (self persistent-children-mixin)
+     &optional (order (default-persist-child-order child self)))
+  (setf (gethash child (slot-value self 'children)) order))
 
-(defgeneric persist-children (store self)
+(defgeneric persist-children (store self order)
   (:documentation "Access and persist `persistent-children' of SELF,
 then remove them from the list.  This is always done by calling
-`persist-child'."))
+`persist-child'.  ORDER indicates which should be taken."))
 
-(defmethod persist-children (store (self persistent-children-mixin))
+(defmethod persist-children (store (self persistent-children-mixin) order)
   "I provide the unneeded but nice additional feature that if any
-persister exits non-locally, it and the remaining children are left in
-the queue for a re-call of `persist-children'."
-  (with-slots (children children-tail) self
-    (let ((remaining children))
-    (unwind-protect (loop :while remaining
-			  :do (persist-child store (car remaining) self)
-			  :do (setf remaining (cdr remaining)))
-      (setf children remaining))
-    (setf children-tail '()))))
+persister exits non-locally, only it and the remaining children are
+left in the queue for a re-call of `persist-children'."
+  (let ((removable '()) (children (slot-value self 'children)))
+    (unwind-protect (progn
+		      (maphash (lambda (child child-order)
+				 (when (member child-order `(,order t))
+				   (persist-child store child self order)
+				   (push child removable)))
+			       children))
+      (dolist (rm removable)
+	(remhash rm children)))))
 
-(defgeneric persist-child (store child parent)
+(defgeneric persist-child (store child parent order)
   (:documentation "Persist CHILD as a consequence of PARENT.  The
 default is to call `persist-object'."))
 
-(defmethod persist-child (store child (parent persistent-children-mixin))
+(defmethod persist-child (store child (parent persistent-children-mixin) order)
+  (declare (ignore parent order))
   (weblocks:persist-object store child))
+
+(defgeneric default-persist-child-order (child parent)
+  (:documentation "Answer the time to persist CHILD relative to PARENT
+when no explicit order is given to `add-child-to-persist'.
+
+I intentionally fail to provide a default, because there isn't a good
+one.  Use `define-default-persist-child-order' to make one
+yourself."))
+
+(defmacro define-default-persist-child-order (child-specializer parent-specializer expr)
+  (arnesi:with-unique-names (child parent)
+    `(defmethod default-persist-child-order ((,child ,child-specializer)
+					     (,parent ,parent-specializer))
+       ,expr)))
+
+(defmethod weblocks:persist-object :before (store (object persistent-children-mixin))
+  "Glue persistent children to the persistence API."
+  (persist-children store object :before))
 
 (defmethod weblocks:persist-object :after (store (object persistent-children-mixin))
   "Glue persistent children to the persistence API."
-  (persist-children store object))
+  (persist-children store object :after))
 
 ;;; persist-children.lisp ends here
