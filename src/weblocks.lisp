42eaff34749230a42dd8de2efdc461b9374e0d66
Yarek Kowalik <yarek.kowalik@gmail.com>
2008-10-16 15:16:55 -0700

    Removed :anaphora from USE expression as is causes conflict problems with metatilities on SBCL 1.0.12.

--- src/weblocks.lisp
+++ src/weblocks.lisp
@@ -1,7 +1,7 @@
 ;;; Code shared accross the entire weblocks framework
 (defpackage #:weblocks
   (:use :cl :c2mop :metabang.utilities :moptilities :hunchentoot :cl-who :json :fare-matcher :cont :parenscript
-        :anaphora :f-underscore)
+        :f-underscore)
   (:shadowing-import-from :c2mop #:defclass #:defgeneric #:defmethod
 			  #:standard-generic-function #:ensure-generic-function
 			  #:standard-class #:typep #:subtypep)
