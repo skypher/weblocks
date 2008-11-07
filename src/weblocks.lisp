58805ce2be90e44bf77b257ce119bbe5fc9db262
Stephen Compall <scompall@nocandysw.com>
2008-11-07 14:04:12 -0600

    replace shadowing import from c2mop removed in r1032:c4d02a0af349; it is needed to load in CCL, as c2mop shadows some CL definitions

--- src/weblocks.lisp
+++ src/weblocks.lisp
@@ -9,6 +9,9 @@
   (defpackage #:weblocks
     (:use :cl :c2mop :metabang.utilities :hunchentoot :cl-who :json :fare-matcher :cont :parenscript
           :anaphora :f-underscore)
+    (:shadowing-import-from :c2mop #:defclass #:defgeneric #:defmethod
+                            #:standard-generic-function #:ensure-generic-function
+                            #:standard-class #:typep #:subtypep)
     (:shadowing-import-from :f-underscore #:f #:_)
     (:shadowing-import-from :fare-matcher #:match)
     (:shadow #:redirect #:errors)
