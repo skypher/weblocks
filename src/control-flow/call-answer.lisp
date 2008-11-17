082aba9910f7947fa6f1baa04baec6bc96b70153
Leslie P. Polzer <polzer@gnu.org>
2008-11-17 11:17:26 +0100

    some docstring cleanups/additions in call-answer.lisp

--- src/control-flow/call-answer.lisp
+++ src/control-flow/call-answer.lisp
@@ -35,12 +35,15 @@
 (defun/cc call (callee op)
   "Saves the current continuation to the appropriate place and
 interrupts the computation. Before the computation is interrupted,
-calls 'op' with the value of the widget where the computation is
-saved (may be different from 'callee' because of objects like
-functions). If 'callee' is of class 'widget', the continuation is
-saved in the 'current-continuation' slot of 'callee'. If 'callee' is a
-function, continuation is curried as a first parameter and the result
-is returned. Otherwise, continuation isn't saved."
+calls OP with the value of the widget where the computation is
+saved (may be different from CALLEE because of objects like
+functions).
+
+If CALLEE is of class WIDGET, the continuation is
+saved in the 'current-continuation' slot of CALLEE.
+
+If CALLEE is a function, continuation is curried as a first parameter
+and the result is returned. Otherwise the continuation isn't passed."
   (let/cc k
     (setf (widget-continuation callee) k)
     (safe-funcall op (if (functionp callee)
@@ -49,9 +52,9 @@ is returned. Otherwise, continuation isn't saved."
     t))
 
 (defun answer (continuation &optional result)
-  "Returns control to location saved in 'continuation', which may be a
+  "Returns control to location saved in CONTINUATION, which may be a
 callee widget or the continuation object explicitly. Continuation is
-called with 'result', or nil. If the widget doesn't have a
+called with RESULT (defaulting to NIL). If the widget doesn't have a
 continuation, recursively tries its parents."
   (if (widget-continuation continuation)
       (safe-funcall (widget-continuation continuation) result)
@@ -68,13 +71,14 @@ WIDGET already has a parent (even if it's PARENT)."
 	(setf (widget-parent widget) parent)))
   (values))
 
-;; Places 'callee' in the place of 'widget', saves the continuation,
-;; and returns from the delimited computation. When 'callee' answers,
-;; restores the original widget and reactivates the computation. If
-;; 'wrapper-fn' is present, passes it the new callee and sets the return
-;; value as the value of a place. By default 'wrapper-fn' is simply an
-;; identity function.
 (defun/cc do-widget (widget callee &optional (wrapper-fn #'identity))
+  "Places CALLEE in the place of WIDGET, saves the continuation,
+and returns from the delimited computation. When CALLEE answers,
+restores the original widget and resumes the computation.
+
+If WRAPPER-FN is present, passes it the new callee and sets the return
+value as the value of a place. By default WRAPPER-FN is simply the
+identity function."
   (if (or (null widget)
 	  (eq widget (root-composite)))
       (do-root-widget callee wrapper-fn)
@@ -89,11 +93,14 @@ WIDGET already has a parent (even if it's PARENT)."
 	(call callee
 	      (lambda (new-callee)
 		(funcall place-writer (funcall wrapper-fn new-callee))))
+      ;; the following is the rest of the computation 
       (funcall place-writer widget))))
 
 ;; This function is aware of the internal structure of the root composite;
 ;; this should be OK as it's a special case; it does violate the contract.
 (defun/cc do-root-widget (callee  &optional (wrapper-fn #'identity))
+  "Replace the contents of the root composite with CALLEE,
+the latter one being optinonally transformed by WRAPPER-FN."
   (let* ((old-value (composite-widgets (root-composite))))
     (prog1
 	(call callee
@@ -104,15 +111,16 @@ WIDGET already has a parent (even if it's PARENT)."
 	    old-value))))
 
 
-;; Sets 'callee' as the only widget in the root composite, saves the
-;; continuation, and returns from the delimited computation. When
-;; 'callee' answers, restores the original widgets in the root
-;; composite and reactivates the computation.
 (defun/cc do-page (callee)
+  "Sets CALLEE as the only widget in the root composite, saves the
+continuation, and returns from the delimited computation. When
+CALLEE answers, restores the original widgets in the root
+composite and reactivates the computation."
   (do-widget nil callee))
 
 (defun/cc do-modal (title callee &key css-class)
-  "Same as 'do-page', but wraps the callee in a div for styling purposes."
+  "Same as DO-PAGE, but wraps CALLEE in a div container
+for styling purposes."
   (do-widget nil callee
 	     (lambda (new-callee)
 	       (lambda (&rest args)
