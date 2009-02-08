d4fc89b9a9acfabf60a7d94e6385f5337ac01bb5
Benjamin Collins <aggieben@gmail.com>
2009-02-08 16:11:54 -0600

    restructure directory.  No need for the tutorial 'blog-record-*' files anymore, since I'll be doing a screencast (and I'll write the steps in separate documentation).  Move everything up to the root level from blog-v4 so it looks like a stock weblocks app.
    committer: Benjamin Collins <ben.collins@acm.org>

--- blog-v0/conf/stores.lisp
+++
@@ -1,9 +0,0 @@
-
-(in-package :blog)
-
-;;; Multiple stores may be defined. The last defined store will be the
-;;; default.
-(defstore *blog-store* :prevalence
-  (merge-pathnames (make-pathname :directory '(:relative "data"))
-		   (asdf-system-directory :blog-v0)))
-
--- blog-v1/src/views.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :blog)
-
-(defview user-table-view (:type table :inherit-from '(:scaffold user)))
-(defview user-data-view (:type data :inherit-from '(:scaffold user)))
-(defview user-form-view (:type form :inherit-from '(:scaffold user)))
-
-(defview post-table-view (:type table :inherit-from '(:scaffold post)))
-(defview post-data-view (:type data :inherit-from '(:scaffold post)))
-(defview post-form-view (:type form :inherit-from '(:scaffold post)))
