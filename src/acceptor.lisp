3e2f78e262406816dadfc3e7320940be537a75e3
Leslie P. Polzer <polzer@gnu.org>
2009-02-26 09:00:09 +0100

    only reset the session secret when necessary

--- src/acceptor.lisp
+++ src/acceptor.lisp
@@ -8,6 +8,12 @@
                         :initarg :session-cookie-name
                         :initform (format nil "weblocks-~(~A~)" (gensym)))))
 
+(defmethod initialize-instance :after ((inst weblocks-acceptor) &rest initargs)
+  "Set the session secret to prevent a Hunchentoot warning emitted upon
+  starting the acceptor."
+  (unless (boundp 'hunchentoot:*session-secret*)
+    (hunchentoot:reset-session-secret)))
+
 (defmethod process-connection ((acceptor weblocks-acceptor) socket)
   (let ((*print-readably* nil))
     (call-next-method)))
