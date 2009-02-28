84f72cad284873d4fa2be10d92acb426c17689db
Jan Rychter <jan@rychter.com>
2009-02-28 09:57:14 +0100

    reintroduce composite's make-widget-place-writer method lost during merges ---  src/widgets/composite.lisp |   26 ++++++++++++++++++++++++++  1 files changed, 26 insertions(+), 0 deletions(-)

--- src/widgets/composite.lisp
+++ src/widgets/composite.lisp
@@ -22,6 +22,32 @@
         (apply #'call-next-method obj :children (append children widgets) initargs))
       (call-next-method)))
 
+(defun cons-in-list-p (cell list)
+  "Simple test that the cell is still part of list to validate the place
+   stored in the closure"
+  (cond ((null list) nil)
+        ((eq list cell) t)
+        (t (cons-in-list-p cell (cdr list)))))
+
+(defmethod make-widget-place-writer ((composite composite) child)
+  "Return a function encapsulating the list where the child is in the car
+   of the first element."
+  (let ((place (member child (composite-widgets composite))))
+    (if place
+      (lambda (&optional (callee nil callee-supplied-p))
+        (assert (cons-in-list-p place (composite-widgets composite)))
+        (cond
+          (callee-supplied-p
+            (check-type callee widget-designator
+                        "a potential member of a composite")
+            (rplaca place callee)
+            (setf (widget-parent callee) composite)
+            (mark-dirty composite))
+          (t (car place))))
+      (lambda (&rest args)
+        (style-warn 'widget-not-in-parent :widget child :parent composite)
+        (mark-dirty composite)))))
+
 ;;; backwards compatibility
 (defmethod composite-widgets ((comp composite))
   (widget-children comp))
