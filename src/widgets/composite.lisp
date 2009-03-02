bbda259e191e3e1bccd9b1f199966d2b7fe5fe5a
Leslie P. Polzer <polzer@gnu.org>
2009-03-02 21:10:56 +0100

    Enhanced/fixed COMPOSITE compatibility interface

--- src/widgets/composite.lisp
+++ src/widgets/composite.lisp
@@ -11,11 +11,14 @@
 (defwidget composite (widget)
   ())
 
-(defmethod composite-widgets ((comp composite))
-  (widget-children comp))
+(defmethod initialize-instance :around ((obj composite) &rest initargs &key widgets &allow-other-keys)
+  (remf initargs :widgets)
+  (apply #'call-next-method obj :children widgets initargs))
 
-(defmethod (setf composite-widgets) (value (comp composite))
-  (setf (widget-children comp :composite) value))
+(defmethod composite-widgets (comp) (widget-children comp))
+
+(defmethod (setf composite-widgets) (value comp)
+  (setf (widget-children comp) value))
 
 (defmacro root-composite ()
   "Expands to code that can be used as a place to access to the root
