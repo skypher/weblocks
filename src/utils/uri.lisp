311ac4bf4ad6b4cde78ad79ec8e3659453912340
Leslie P. Polzer <polzer@gnu.org>
2009-06-20 11:57:26 +0200

    added missing file src/utils/uri.lisp

---
+++ src/utils/uri.lisp
@@ -0,0 +1,68 @@
+
+(in-package :weblocks)
+
+(wexport '(request-uri-path
+           add-get-param-to-url
+	   remove-parameter-from-uri)
+	 '(t util))
+
+;;; URI from pathname
+(defmethod puri:uri ((thing pathname))
+  (puri:uri
+   (format nil "~A~{~A/~}~A~A"
+	   (if (eql (car (pathname-directory thing)) :absolute) "/" "")
+	   (cdr (pathname-directory thing))
+	   (or (pathname-name thing) "")
+	   (if (pathname-type thing)
+	       (format nil ".~A" (pathname-type thing))
+	       ""))))
+
+(defun request-uri-path ()
+  "Returns the path component of the request URI. The path component
+does not include the domain name, and any query string parameters.
+Ex (when URI is http://blah.com/foo/bar?x=1&y=2):
+\(request-uri-path)
+=> \"/foo/bar\""
+  (identity (cl-ppcre:regex-replace "/?(?:\\?.*)$" (request-uri*) "")))
+
+
+(defun add-get-param-to-url (url name value)
+  "Based on Edi's code in URL-REWRITE but uses & instead of &amp;
+which is more appropriate for our uses."
+  (concatenate 'string
+               url
+               (if (find #\? url :test #'char=)
+                 "&"
+                 "?")
+               name
+               "="
+               (url-rewrite:url-encode value)))
+
+(defun remove-parameter-from-uri (uri parameter)
+  "Removes the given parameter from a URI."
+  (let ((path (puri:uri-path (puri:parse-uri uri))))
+    (loop for x in (get-parameters*)
+       when (not (string-equal (car x) parameter))
+       do (setf path (add-get-param-to-url path (car x) (cdr x))))
+    path))
+
+
+(defun tokenize-uri (uri &optional (remove-app-prefix t) (app (when remove-app-prefix
+                                                                (current-webapp))))
+  "Tokenizes an URI into a list of elements.
+
+ex:
+\(tokenize-uri \"/hello/world/blah\\test\\hala/world?hello=5;blah=7\"
+=> (\"hello\" \"world\" \"blah\" \"test\" \"hala\" \"world\")"
+  (loop for token in (cl-ppcre:split "[/\\\\]" 
+				     (cl-ppcre:regex-replace "\\?.*"
+							     (if remove-app-prefix
+								 (subseq uri
+                                                                         (length
+                                                                           (webapp-prefix
+                                                                             app)))
+								 uri)
+							     ""))
+     unless (string-equal "" token)
+       collect (url-decode token)))
+
