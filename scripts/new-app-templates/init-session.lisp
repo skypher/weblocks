36f1e7e125c64e1d711ad9ecadbf1629e0c50869
Leslie P. Polzer <polzer@gnu.org>
2010-04-22 09:26:57 +0200

    New app template: Use widget-children instead of the deprecated composite-widgets.

--- scripts/new-app-templates/init-session.lisp
+++ scripts/new-app-templates/init-session.lisp
@@ -2,8 +2,8 @@
 (in-package :{APPNAME})
 
 ;; Define callback function to initialize new sessions
-(defun init-user-session (comp)
-  (setf (composite-widgets comp)
+(defun init-user-session (root)
+  (setf (widget-children root)
 	(list (lambda (&rest args)
 		(with-html
 		  (:strong "Happy Hacking!"))))))
