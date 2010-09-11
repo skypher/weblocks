6e4386bd1eebecff3c8fc4626d42994a2d65228f
Leslie P. Polzer <polzer@gnu.org>
2010-09-11 13:28:25 +0200

    Fix a new symbol conflict between ps:str and cl-who:str.

--- src/package.lisp
+++ src/package.lisp
@@ -10,6 +10,7 @@
     (:shadowing-import-from :c2mop #:defclass #:defgeneric #:defmethod
                             #:standard-generic-function #:ensure-generic-function
                             #:standard-class #:typep #:subtypep #:standard-method)
+    (:shadowing-import-from :cl-who #:str)
     (:shadowing-import-from :f-underscore #:f #:_)
     (:shadowing-import-from :fare-matcher #:match)
     (:shadowing-import-from :metabang.utilities #:with-array)
