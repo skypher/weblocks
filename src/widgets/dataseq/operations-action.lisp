09e92967dfa0e9dc8ec76dcd473ddc1210951402
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-06-01 14:12:43 +0300

    Fixed dataseq operations with string ids

--- src/widgets/dataseq/operations-action.lisp
+++ src/widgets/dataseq/operations-action.lisp
@@ -8,17 +8,21 @@
 ;;; ASDF for all implementations except CMUCL, and for CMUCL this file
 ;;; will be loaded without compilation.
 (defun/cc dataseq-operations-action (obj &rest args)
-  (declare (ignore args))
-  (dataseq-clear-selection obj)
-  (loop for i in (request-parameters)
-     when (string-starts-with (car i) "item-")
-     do (dataseq-select-item obj (parse-integer (substring (car i) 5))))
-  (loop for i in (append
-		  (dataseq-item-ops obj)
-		  (dataseq-common-ops obj))
-        when (member (attributize-name (car i)) (request-parameters)
-                     :key (compose #'attributize-name #'car)
-                     :test #'string-equal)
-          do (funcall (cdr i) obj (dataseq-selection obj)))
-  (dataseq-clear-selection obj))
+          (declare (ignore args))
+          (dataseq-clear-selection obj)
+          (let ((id))
+            (loop for i in (request-parameters)
+                  when (string-starts-with (car i) "item-")
+                  do 
+                  (setf id (substring (car i) 5))
+                  (setf id (if (ppcre:scan "^\\d+$" id) (parse-integer id) id))
+                  (dataseq-select-item obj id)))
+          (loop for i in (append
+                           (dataseq-item-ops obj)
+                           (dataseq-common-ops obj))
+                when (member (attributize-name (car i)) (request-parameters)
+                             :key (compose #'attributize-name #'car)
+                             :test #'string-equal)
+                do (funcall (cdr i) obj (dataseq-selection obj)))
+          (dataseq-clear-selection obj))
 
