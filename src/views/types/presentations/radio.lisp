9610ecf5351fced1400ad6a7ef15a1fa8dcd198b
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-08-18 21:59:28 +0300

    Separated view from code for radio presentation

--- src/views/types/presentations/radio.lisp
+++ src/views/types/presentations/radio.lisp
@@ -7,34 +7,60 @@
 (defclass radio-presentation (form-presentation choices-presentation-mixin)
   ())
 
+(defun radio-view-field-wt (&key field-class content field-label show-required-indicator required-indicator-label validation-error)
+  (with-html-to-string
+    (:li :class field-class
+     (:span :class "label"
+      (:span :class "slot-name"
+       (:span :class "extra"
+        (str field-label) ":&nbsp;"
+        (when show-required-indicator 
+          (htm 
+            (:em :class "required-slot"
+             (str required-indicator-label)
+             (str "&nbsp;")))))))
+     (str content)
+     (when validation-error
+       (htm (:p :class "validation-error"
+             (:em
+               (:span :class "validation-error-heading" "Error:&nbsp;")
+               (str validation-error))))))))
+
+(deftemplate :radio-view-field-wt 'radio-view-field-wt)
+
 (defmethod render-view-field ((field form-view-field) (view form-view)
-			      widget (presentation radio-presentation) value obj
-			      &rest args &key validation-errors field-info &allow-other-keys)
+                                                      widget (presentation radio-presentation) value obj
+                                                      &rest args &key validation-errors field-info &allow-other-keys)
   (let* ((attribute-slot-name (if field-info
                                 (attributize-view-field-name field-info)
                                 (attributize-name (view-field-slot-name field))))
-	 (validation-error (assoc attribute-slot-name (remove-if (lambda (item) (null (car item))) validation-errors)
-				  :test #'string-equal
-				  :key (lambda (f) (and f (view-field-slot-name f)))))
-	 (field-class (concatenate 'string attribute-slot-name
-				   (when validation-error " item-not-validated"))))
-    (with-html
-      (:li :class field-class
-	   (:span :class "label"
-		  (:span :class "slot-name"
-			 (:span :class "extra"
-				(str (view-field-label field)) ":&nbsp;"
-				(render-form-view-field-required-indicator
-				  field view widget presentation value obj))))
-	   (apply #'render-view-field-value
-		  value presentation
-		  field view widget obj
-		  args)
-	   (when validation-error
-	     (htm (:p :class "validation-error"
-		      (:em
-		       (:span :class "validation-error-heading" "Error:&nbsp;")
-		       (str (format nil "~A" (cdr validation-error)))))))))))
+         (validation-error (assoc attribute-slot-name (remove-if (lambda (item) (null (car item))) validation-errors)
+                                  :test #'string-equal
+                                  :key (lambda (f) (and f (view-field-slot-name f)))))
+         (field-class (concatenate 'string attribute-slot-name
+                                   (when validation-error " item-not-validated")))
+         (required-indicator (form-view-field-required-indicator field))
+         (show-required-indicator (and (form-view-field-required-p field)
+                                       (not (form-view-field-disabled-p field obj))
+                                       required-indicator)))
+    (write-string 
+      (render-template-to-string 
+        :radio-view-field-wt 
+        (list :field field :view view :widget widget :presentation presentation :object obj :value value)
+        :field-class field-class 
+        :field-label (translate (view-field-label field))
+        :show-required-indicator show-required-indicator
+        :required-indicator-label (when show-required-indicator 
+                                    (if (eq t required-indicator)
+                                      *default-required-indicator*
+                                      required-indicator))
+        :validation-error (and validation-error (format nil "~A" (cdr validation-error)))
+        :content (capture-weblocks-output 
+                   (apply #'render-view-field-value
+                          value presentation
+                          field view widget obj
+                          args)))
+      *weblocks-output-stream*)))
 
 (defmethod render-view-field-value (value (presentation radio-presentation)
 				    (field form-view-field) (view form-view) widget obj
