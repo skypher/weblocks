70c065ac68836a6bf9c3b8dcc4a9248d8e93e9a4
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-08-30 21:09:02 +0300

    Improved deftemplate behaviour, added remove-template

--- src/utils/templates.lisp
+++ src/utils/templates.lisp
@@ -1,13 +1,33 @@
 (in-package :weblocks-util)
 
-(wexport '(*templates* deftemplate render-template-to-string)
+(wexport '(*templates* remove-template deftemplate render-template-to-string)
          '(t util))
 
 (defvar *templates* (make-hash-table))
+(setf (documentation '*templates* 'variable)
+      "Hash containing all templates. Hash key is template name, hash value is a cons with template and context arguments")
+
+(defun remove-template (name function)
+  "Removes template associated with name and function."
+  (let ((templates (gethash name *templates*)))
+    (setf templates 
+          (remove function  templates
+                  :key #'car
+                  :test #'equal))
+    (setf (gethash name *templates*) templates)))
 
 (defun deftemplate (name function &rest context)
-  (push (cons function context)
-        (gethash name *templates*)))
+  "Associates function with template name. Template name is a keyword.
+   Context arguments used to calculate effective template.
+   :application-class, a symbol, this argument gives 10 points for template if its value equal to type of application used.
+   :context-matches argument is a callback which will be called with template context during template calculation and should return points"
+  (let ((templates (gethash name *templates*)))
+
+    ; Handling redefining template
+    (remove-template name function)
+
+    (push (cons function context) templates)
+    (setf (gethash name *templates*) templates)))
 
 (defun calculate-template-priority (template actual-context)
   "Calculates priority for finding effective template."
--- weblocks.asd
+++ weblocks.asd
@@ -8,7 +8,7 @@
 
 (defsystem weblocks
    :name "weblocks"
-   :version "0.9.42"
+   :version "0.9.43"
    :maintainer "Olexiy Zamkoviy, Scott L. Burson"
    :author "Slava Akhmechet"
    :licence "LLGPL"
