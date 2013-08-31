c159a5244ab05accad1c06cfe166ffdee297e78d
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-08-31 14:37:47 +0300

    Separated page views from code

--- src/page-template.lisp
+++ src/page-template.lisp
@@ -54,25 +54,34 @@
 the HTML already rendered to *weblocks-output-stream* with boilerplate
 page HTML (title, stylesheets, etc.).  Can be overridden by subclasses"))
 
+(defun page-wt (&key title header-content body-content)
+  (with-html-output-to-string (*weblocks-output-stream* nil :prologue t)
+    (:html :xmlns "http://www.w3.org/1999/xhtml"
+     (:head
+       (:title (str title))
+       (str header-content))
+     (:body
+       (str body-content)
+       (:div :id "ajax-progress" "&nbsp;")
+       (with-javascript "updateWidgetStateFromHash();")))))
+
+(deftemplate :page-wt 'page-wt)
+
 (defmethod render-page ((app weblocks-webapp))
   "Default page rendering template and protocol"
   ; Note, anything that precedes the doctype puts IE6 in quirks mode
   ; (format *weblocks-output-stream* "<?xml version=\"1.0\" encoding=\"utf-8\" ?>")
   (declare (special *page-dependencies*))
   (let ((rendered-html (get-output-stream-string *weblocks-output-stream*))
-	(all-dependencies (timing "compact-dependencies"
+        (all-dependencies (timing "compact-dependencies"
                             (compact-dependencies (append (webapp-application-dependencies)
                                                           *page-dependencies*)))))
-    (with-html-output (*weblocks-output-stream* nil :prologue t)
-      (:html :xmlns "http://www.w3.org/1999/xhtml"
-	     (:head
-	      (:title (str (application-page-title app)))
-	      (render-page-headers app)
-	      (mapc #'render-dependency-in-page-head all-dependencies))
-	     (:body
-	      (render-page-body app rendered-html)
-	      (:div :id "ajax-progress" "&nbsp;")
-              (with-javascript "updateWidgetStateFromHash();"))))))
+    (render-wt :page-wt (list :app app) 
+               :title (application-page-title app)
+               :header-content (capture-weblocks-output 
+                                 (render-page-headers app)
+                                 (mapc #'render-dependency-in-page-head all-dependencies))
+               :body-content (capture-weblocks-output (render-page-body app rendered-html)))))
 
 ;;
 ;; Render header entries
@@ -108,10 +117,15 @@ methods to render extra html prior and post the page wrapper and override
 the main method to replace the generation of the page wrapper and any contents
 inside it but outside the rendering of the root widget"))
 
+(defun page-body-wt (&key body-string &allow-other-keys)
+  (with-html-to-string
+    (:div :class "page-wrapper"
+     (render-extra-tags "page-extra-top-" 3)
+     (htm (str body-string))
+     (render-extra-tags "page-extra-bottom-" 3))))
+
+(deftemplate :page-body-wt 'page-body-wt)
+
 (defmethod render-page-body ((app weblocks-webapp) body-string)
   "Default page-body rendering method"
-  (with-html
-    (:div :class "page-wrapper"
-	  (render-extra-tags "page-extra-top-" 3)
-	  (htm (str body-string))
-	  (render-extra-tags "page-extra-bottom-" 3))))
+  (render-wt :page-body-wt (list :app app) :body-string body-string))
--- weblocks.asd
+++ weblocks.asd
@@ -8,7 +8,7 @@
 
 (defsystem weblocks
    :name "weblocks"
-   :version "0.9.45"
+   :version "0.9.46"
    :maintainer "Olexiy Zamkoviy, Scott L. Burson"
    :author "Slava Akhmechet"
    :licence "LLGPL"
