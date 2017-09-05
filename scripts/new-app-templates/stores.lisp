38fbb7a16174987a6185ce5ce489a31963345b37
Alexander Artemenko <svetlyak.40wt@gmail.com>
2017-09-05 23:42:28 +0300

    Removed scripts/make-new-app.lisp because it breaks Qlot usage.

--- scripts/new-app-templates/stores.lisp
+++
@@ -1,9 +0,0 @@
-
-(in-package :{APPNAME})
-
-;;; Multiple stores may be defined. The last defined store will be the
-;;; default.
-(defstore *{APPNAME}-store* :prevalence
-  (merge-pathnames (make-pathname :directory '(:relative "data"))
-                   (asdf-system-directory :{APPNAME})))
-
