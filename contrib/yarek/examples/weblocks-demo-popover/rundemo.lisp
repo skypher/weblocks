a32afe7c006e5822db90cf4a65f00b97a567c102
Alexander Artemenko <svetlyak.40wt@gmail.com>
2018-01-13 11:04:11 +0300

    All contribs were dropped.

--- contrib/yarek/examples/weblocks-demo-popover/rundemo.lisp
+++
@@ -1,4 +0,0 @@
-;(push #p"/usr/lib/sbcl/site/cl-weblocks/" asdf:*central-registry*)
-(push #p"/path/to/the/demo/root/dir/weblocks-demo-popover/" asdf:*central-registry*)
-(asdf:operate 'asdf:load-op :weblocks-demo-popover)
-(weblocks-demo-popover:start-weblocks-demo-popover :debug t)
