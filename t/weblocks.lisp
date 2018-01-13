55ee71b069062fbffd07ecf68ffd9b6e324a962c
Alexander Artemenko <svetlyak.40wt@gmail.com>
2018-01-13 11:50:04 +0300

    Removed tests for composite widget, because widget itself was removed.

--- t/weblocks.lisp
+++ t/weblocks.lisp
@@ -5,22 +5,7 @@
         #:weblocks.t.utils))
 (in-package weblocks.t.weblocks)
 
-(plan 3)
-
-(subtest "root-composite-1"
-  (with-session
-    (is-values (weblocks::root-composite)
-               (list nil nil)
-               "root-composite by default returns two nil values")))
-
-
-(subtest "root-composite-2"
-  (with-session
-    (setf (weblocks::root-composite) 'foobar)
-    (is-values (weblocks::root-composite)
-               (list 'foobar t)
-               "setf for root-composite should put any value to the session hash")))
-
+(plan 1)
 
 (subtest "with-javascript-1"
     (is
