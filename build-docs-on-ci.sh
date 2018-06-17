2d72c8babe5c3172aefe15656ad78b03f30ce8b3
Alexander Artemenko <svetlyak.40wt@gmail.com>
2018-06-17 16:02:48 +0300

    Additional check for TRAVIS_PULL_REQUEST=false.

--- build-docs-on-ci.sh
+++ build-docs-on-ci.sh
@@ -4,7 +4,13 @@ set -e
 
 # we only want to build documentation from the "reblocks" branch
 # and want to make it only once.
-if [ "$TRAVIS_BRANCH" = "reblocks" -a "$LISP" = "ccl" ]; then
+# Also, we need to check if TRAVIS_PULL_REQUEST=false because Travis
+# builds which are running for pull requests will have
+# TRAVIS_BRANCH=reblocks, but TRAVIS_PULL_REQUEST_BRANCH=the-branch
+# and TRAVIS_PULL_REQUEST=42 where 42 is a pull request number
+if [ "$TRAVIS_BRANCH" = "reblocks" -a "$TRAVIS_PULL_REQUEST" = "false" -a "$LISP" = "ccl" ]; then
     sudo pip install -r docs/requirements.txt
     ./build-docs.ros
+else
+    echo "Skipping documentation build because environment is not suitable."
 fi
